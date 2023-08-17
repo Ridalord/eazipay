397cd91c14f6c7113e0c4ea5ba7e66351e59bcc9
Ridalord <ridhwan.alabi@gmail.com>
2023-08-17 07:22:11 +0100

    How section complete

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./index.module.css"
 import IntroSection from "./introSection";
 import AboutSection from "./aboutSection";
+import HowSection from "./howSection";
 
 
 
@@ -9,13 +10,15 @@ export default function Home() {
   return (
     <div className={classes.homeWrap}>
       <IntroSection />
-      <AboutSection/>
+      <AboutSection />
+      <HowSection/>
       <img className={`${classes.ellipse} ${classes.ellipse4}`} src="./group17.png" alt="Ellipse 4" />
       <img className={`${classes.ellipse} ${classes.ellipse3}`} src="./group16.png" alt="Ellipse 3" />
       <img className={`${classes.ellipse} ${classes.ellipse2}`} src="./group18.png" alt="Ellipse 2" />
       <img className={`${classes.ellipse} ${classes.ellipse1}`} src="./group19.png" alt="Ellipse 1" />
-      <img className={`${classes.ellipse} ${classes.pattern}`} src="./patterns.png" alt="Pattern" />
+      <img className={`${classes.ellipse} ${classes.pattern1}`} src="./patterns.png" alt="Pattern" />
       <img className={`${classes.ellipse} ${classes.pattern2}`} src="./patterns2.png" alt="Pattern" />
+      <img className={`${classes.ellipse} ${classes.pattern3}`} src="./patterns3.png" alt="Pattern" />
     </div>
   )
 }
